1bc2daa1ee0367387516715d9001a98ce232f31d
Sapan Parikh <sapanparikh18@gmail.com>
2016-05-15 16:38:17 +0530

    stream roller

--- firepano.js
+++ firepano.js
@@ -1,5 +1,5 @@
 var spinner = new Spinner({color: '#ddd'});
-var firebaseRef = 'https://firepano.firebaseio.com/';
+var firebaseRef = 'https://vivid-heat-953.firebaseio.com/';
 
 function handleFileSelect(evt) {
   var f = evt.target.files[0];
@@ -8,43 +8,66 @@ function handleFileSelect(evt) {
     return function(e) {
       var filePayload = e.target.result;
       // Generate a location that can't be guessed using the file's contents and a random number
-      var hash = CryptoJS.SHA256(Math.random() + CryptoJS.SHA256(filePayload));
-      var f = new Firebase(firebaseRef + 'pano/' + hash + '/filePayload');
+      //var hash = CryptoJS.SHA256(Math.random() + CryptoJS.SHA256(filePayload));
+      var f = new Firebase(firebaseRef + 'photo/stream' );
       spinner.spin(document.getElementById('spin'));
       // Set the file payload to Firebase and register an onComplete handler to stop the spinner and show the preview
-      f.set(filePayload, function() { 
+      f.push(filePayload, function() {
         spinner.stop();
         document.getElementById("pano").src = e.target.result;
-        $('#file-upload').hide();
+       // $('#file-upload').hide();
         // Update the location bar so the URL can be shared with others
-        window.location.hash = hash;
+       // window.location.hash = hash;
       });
     };
   })(f);
   reader.readAsDataURL(f);
 }
+function requestFullScreen(element) {
 
-$(function() {
+    // Supports most browsers and their versions.
+    var requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullScreen;
+    if (requestMethod) { // Native full screen.
+        requestMethod.call(element);
+    } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
+        var wscript = new ActiveXObject("WScript.Shell");
+        if (wscript !== null) {
+            wscript.SendKeys("{F11}");
+        }
+    }
+    $("#inputDiv").remove();
+    startStreaming();
+}
+ function startStreaming() {
   $('#spin').append(spinner);
+    var i = 0;
+        // A hash was passed in, so let's retrieve and render it.
+        spinner.spin(document.getElementById('spin'));
+        var f = new Firebase(firebaseRef + '/photo/stream');
+        f.on('child_added', function (snap) {
+            var payload = snap.val();
+            if (payload != null) {
+                $("#"+i).appendTo("#archive");
+                var img = $('<img>'); //Equivalent: $(document.createElement('img'))
+                var child = $('<div/>');
+                child.addClass('content');
+                var parent = $('<div id="'+(++i)+'"/>');
+                parent.addClass('area');
+                parent.html(child);
+                child.html(img);
+                child.css("height",$(window).height());
+                img.attr('src', payload);
+                $('#display').prepend(parent);
+                if($(img).height()>$(img).width()){
+                    img.css("height",  $(window).height()+ "px");
+                }else{
+                    img.css("width",  $(window).width() + "px");
+                }
+                window.location.hash = "#"+i;
+            } else {
+                $('#body').append("Not found");
+            }
+            spinner.stop();
+        });
 
-  var idx = window.location.href.indexOf('#');
-  var hash = (idx > 0) ? window.location.href.slice(idx + 1) : '';
-  if (hash === '') {
-    // No hash found, so render the file upload button.
-    $('#file-upload').show();
-    document.getElementById("file-upload").addEventListener('change', handleFileSelect, false);
-  } else {
-    // A hash was passed in, so let's retrieve and render it.
-    spinner.spin(document.getElementById('spin'));
-    var f = new Firebase(firebaseRef + '/pano/' + hash + '/filePayload');
-    f.once('value', function(snap) {
-      var payload = snap.val();
-      if (payload != null) {
-        document.getElementById("pano").src = payload;
-      } else {
-        $('#body').append("Not found");
-      }
-      spinner.stop();
-    });
-  }
-});
+}
